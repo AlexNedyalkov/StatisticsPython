b7c3021e101e139e025fbda96ad6594cf7f20137
AlexNedyalkov <nedyalkov6@gmail.com>
2021-03-11 09:13:18 +0200

    Completed zscore normalization

---
+++ Data_normalization_outliers/z_normalization.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import stats
+
+data = np.random.poisson(3, 1000)**2
+
+
+data_mean = np.mean(data)
+data_std = np.std(data, ddof=1)
+
+plt.plot(data, 's', markersize=3)
+plt.xlabel('Data index')
+plt.ylabel('Data value')
+plt.title(f'Mean = {np.round(data_mean,2)}; std = {np.round(data_std,2)}')
+
+plt.show()
+
+data_z = (data-data_mean) / data_std
+# can also use Python function
+#  = stats.zscore(data)
+
+data_z_mean = np.mean(data_z)
+data_z_std = np.std(data_z, ddof=1)
+
+plt.plot(data_z, 's', markersize=3)
+plt.xlabel('Data index')
+plt.ylabel('Data value')
+plt.title(f'Mean = {np.round(data_z_mean, 2)}; std = {np.round(data_z_std, 2)}')
+
+plt.show()
+
+plt.plot(data, data_z, 's')
+plt.xlabel('Original')
+plt.ylabel('Z-transformed')
+plt.title('Correlation r = %g'%np.corrcoef(data, data_z)[0, 0])
+plt.show()
+
+
